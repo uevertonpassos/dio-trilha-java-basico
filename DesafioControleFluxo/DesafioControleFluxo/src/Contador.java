5dfe26b3315f8a8ad57b9e05bff25b63949bc901
Ueverton <uevertonpassos96@gmail.com>
2023-08-29 13:04:39 -0300

    Desafio Controle de Fluxo JAVA

---
+++ DesafioControleFluxo/DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroDois <= parametroUm) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = parametroUm; i <= parametroDois; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+class ParametrosInvalidosException extends Exception {
+    // Pode deixar esta classe vazia, ou personalizar conforme necessário
+}
